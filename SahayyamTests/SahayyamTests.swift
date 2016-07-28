2ade60de1ff01ee2be64b72eee01c7ef087da2b0
Sanjeev Jikamade <sanjeevjikamade@sanjeevs-mac-mini.local>
2016-07-28 14:59:09 +0530

    Initial Commit

---
+++ SahayyamTests/SahayyamTests.swift
@@ -0,0 +1,36 @@
+//
+//  SahayyamTests.swift
+//  SahayyamTests
+//
+//  Created by Sanjeev Jikamade on 28/07/16.
+//  Copyright © 2016 Sanjeev Jikamade. All rights reserved.
+//
+
+import XCTest
+@testable import Sahayyam
+
+class SahayyamTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
